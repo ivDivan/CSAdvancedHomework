bb103b800e0a63d56e6e059151bbaf3c85de71a3
ivDivan <iv.popovski@gmail.com>
2024-04-25 17:50:49 +0200

    commit

---
+++ Class09Homework/Class09Homework.Homework/Program.cs
@@ -0,0 +1,122 @@
+﻿
+
+//=========Task 01=========
+
+string appPath = @"..\..\..\";
+string folderPath = appPath + @"Files\";
+string filePath = folderPath + @"names.txt";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+    Console.WriteLine("Directory created!!!");
+}
+
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+    Console.WriteLine("File created!!!");
+}
+
+//=========Task 02==========
+
+void ReadFile()
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        string firstRead = sr.ReadToEnd();
+        if (string.IsNullOrEmpty(firstRead))
+        {
+            Console.WriteLine("Nuthin here, mate...");
+        }
+        else
+        {
+            Console.WriteLine(firstRead);
+        }
+    }
+}
+
+
+List<string> names = new List<string>();
+string choiceInput;
+List<string> UserInput()
+{
+    names.Clear();
+
+    do
+    {
+        while (true)
+        {
+            Console.WriteLine("Gimme a name: ");
+            string nameInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(nameInput))
+            {
+                Console.WriteLine("Oi, u need to input a name!!");
+            }
+            else
+            {
+                names.Add(nameInput);
+                break;
+            }
+        }
+
+        Console.WriteLine("Type \"N\" to finish, anything else to write another name.");
+        choiceInput = Console.ReadLine().ToLower();
+            
+        
+    }
+    while (choiceInput != "n");
+
+    return names;
+}
+
+
+void Write(List<string> text, string path)
+{
+    foreach (string s in text)
+    {
+        using (StreamWriter sw = new StreamWriter(path, true))
+        {
+            sw.WriteLine(s);
+        }
+    }
+}
+
+ReadFile();
+UserInput();
+Write(names, filePath);
+
+
+//============Task 03=================
+
+Console.WriteLine("Names in file: ");
+ReadFile();
+
+string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+List<string> charNames = new List<string>();
+
+foreach (char letter in alphabet)
+{
+    string charFilePath = folderPath + @"namesStartingWith" + $@"{letter}";
+    charNames.Clear();
+
+    using(StreamReader sr = new StreamReader(filePath))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (line.StartsWith(letter.ToString().ToUpper()))
+            {
+                charNames.Add(line);
+            }
+        }
+    }
+
+    Write(charNames, charFilePath);
+
+    if(File.Exists(charFilePath) && charNames.Count == 0)
+    {
+        File.Delete(charFilePath);
+    }
+}
